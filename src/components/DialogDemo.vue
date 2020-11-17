9fb317da226bded9d7698e867c4d96449118d8a0
Dusty <wajtejprotl@outlook.com>
2020-11-17 14:01:49 +0800

    dialog 支持 点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,20 +1,42 @@
 <template>
   <div class="dialog">
     <h1>示例1</h1>
-    <Dialog></Dialog>
+    <Button @click="toggle">toggle</Button>
+    <Dialog
+      :visible='visible'
+      @closeDialog='closeDialog'
+      @dialogOk='dialogOk'
+      @dialogCancel='dialogCancel'
+      :closeOnClickOverlay='false'
+    ></Dialog>
   </div>
 </template>
 
 <script lang='ts'>
+import { ref } from "vue";
+import Button from "/@/lib/Button.vue";
 import Dialog from "/@/lib/Dialog.vue";
 export default {
   name: "DialogDemo",
-  components: { Dialog },
+  components: { Dialog, Button },
   props: {
     msg: String,
   },
-  data() {
-    return {};
+  setup() {
+    const visible = ref(false);
+    const toggle = () => {
+      visible.value = !visible.value;
+    };
+    const closeDialog = () => {
+      visible.value = false;
+    };
+    const dialogOk = () => {
+      closeDialog();
+    };
+    const dialogCancel = () => {
+      closeDialog();
+    };
+    return { dialogOk, dialogCancel, visible, toggle, closeDialog };
   },
 };
 </script>
