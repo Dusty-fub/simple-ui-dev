97eb6045af184cbb1e4d7cb166e6759b2efe9005
Dusty <wajtejprotl@outlook.com>
2020-12-13 17:00:23 +0800

    添加上中下布局demo

--- src/components/LayoutDemo.vue
+++ src/components/LayoutDemo.vue
@@ -1,16 +1,18 @@
 <template>
   <Demo :component="LayoutDemo1" />
   <Demo :component="LayoutDemo2" />
+  <Demo :component="LayoutDemo3" />
 </template>
 
 <script lang="ts">
 import Demo from "/src/components/Demo.vue";
 import LayoutDemo1 from "/src/components/demos/layout/Layout.demo1.vue";
 import LayoutDemo2 from "/src/components/demos/layout/Layout.demo2.vue";
+import LayoutDemo3 from "/src/components/demos/layout/Layout.demo3.vue";
 export default {
   components: { Demo },
   setup(props) {
-    return { LayoutDemo1, LayoutDemo2 };
+    return { LayoutDemo1, LayoutDemo2, LayoutDemo3 };
   },
 };
 </script>
---
+++ src/components/demos/layout/Layout.demo3.vue
@@ -0,0 +1,29 @@
+<demo>
+    示例 2
+</demo>
+<template>
+  <div class="layoutDemo">
+    <Layout>
+      <Header>header</Header>
+      <Content>content</Content>
+      <Footer>footer</Footer>
+    </Layout>
+  </div>
+</template>
+
+<script lang="ts">
+import Layout from "/src/lib/Layout.vue";
+import Header from "/src/lib/Header.vue";
+import Content from "/src/lib/Content.vue";
+import Footer from "/src/lib/Footer.vue";
+
+export default {
+  components: { Layout, Header, Content, Footer },
+};
+</script>
+
+<style>
+.layoutDemo {
+  height: 300px;
+}
+</style>
