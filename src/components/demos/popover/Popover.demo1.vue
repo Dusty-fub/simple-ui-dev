72ff5ecde8a093ea320baea49c8ce4eb535a8e89
Dusty <wajtejprotl@outlook.com>
2020-12-17 10:18:58 +0800

    popover 下方弹出实现

--- src/components/demos/popover/Popover.demo1.vue
+++ src/components/demos/popover/Popover.demo1.vue
@@ -2,8 +2,12 @@
     示例 1
 </demo>
 <template>
-  <Popover position="top">
+  <Popover>
     <Button>上方弹出</Button>
+    <template v-slot:content> 要弹出的内容 </template>
+  </Popover>
+  <Popover position="bottom">
+    <Button>下方弹出</Button>
     <template v-slot:content> 要弹出的内容 </template>
   </Popover>
 </template>
--- src/lib/popover/Popover.vue
+++ src/lib/popover/Popover.vue
@@ -3,13 +3,13 @@
     <span @click="toggleContent">
       <slot></slot>
     </span>
-    <div v-if="isContentVisible" class="gulu-popover-content">
+    <div v-if="isContentVisible" :class="classes">
       <slot name="content"></slot>
     </div>
   </div>
 </template>
 <script lang="ts">
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 export default {
   props: {
     position: {
@@ -25,14 +25,19 @@ export default {
     const toggleContent = () => {
       isContentVisible.value = !isContentVisible.value;
     };
-    return { toggleContent, isContentVisible };
+    const classes = reactive({});
+    classes["gulu-popover-content-bottom"] = props.position === "bottom";
+    classes["gulu-popover-content-top"] = props.position === "top";
+
+    return { toggleContent, isContentVisible, classes };
   },
 };
 </script>
 <style lang="scss">
 .gulu-popover {
   position: relative;
-  > .gulu-popover-content {
+
+  > .gulu-popover-content-top {
     position: absolute;
     top: 0px;
     transform: translateY(-120%);
@@ -61,6 +66,38 @@ export default {
       border-bottom: none;
       border-top-color: white;
       top: calc(100% - 1px);
+    }
+  }
+
+  > .gulu-popover-content-bottom {
+    position: absolute;
+    bottom: 0px;
+    transform: translateY(120%);
+    padding: 0.5em;
+    background-color: white;
+    border: 1px solid #333;
+    border-radius: 0.2em;
+    box-shadow: 0 0 9px #ccc;
+
+    &::before,
+    &::after {
+      content: "";
+      display: block;
+      border: 7px solid transparent;
+      width: 0;
+      height: 0;
+      position: absolute;
+      left: 30%;
+    }
+    &::before {
+      border-top: none;
+      border-bottom-color: black;
+      bottom: 100%;
+    }
+    &::after {
+      border-top: none;
+      border-bottom-color: white;
+      bottom: calc(100% - 1px);
     }
   }
 }
